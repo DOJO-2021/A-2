c851ea6750948493813856343dacce39520711d9
dojo2021-y-mayuka <yoshino_dojo2021@seplus2016.onmicrosoft.com>
2021-06-09 16:40:20 +0900

    1

--- OpenQA/WebContent/WEB-INF/jsp/login.jsp
+++ OpenQA/WebContent/WEB-INF/jsp/login.jsp
@@ -1,12 +1,31 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>Open Q&A System</title>
 </head>
 <body>
+	<img src="images/mainLogo.png" alt="">
+
+	<form method="POST" action="/OpenQA/LoginServlet" id="form">
+		<p>
+		    <!-- IDとPWは外に表記？それともプレイスホルダーのみ？ -->
+			<input type="text" name="ID" placeholder="ID">
+		</p>
+		<p>
+			<input type="password" name="PW" placeholder="PW">
+		</p>
+		${errMsg}<!-- 「※IDかPWが間違えています。」のエラーをここで出す。 -->
+		<p>
+			<input type="submit" name="LOGIN" value="ログイン">
+		</p>
+	</form>
+
+	<a href="/OpenQA/RegistServlet">新規会員登録はこちら</a>
+	<br>
+	<a href="/OpenQA/UpdateDeleteServlet">PW再設定はこちら</a>
 
 </body>
 </html>
--- OpenQA/src/model/User.java
+++ OpenQA/src/model/User.java
@@ -3,5 +3,74 @@ package model;
 import java.io.Serializable;
 
 public class User implements Serializable {
+	// JavaBeansの作成
+
+	// フィールドの作成
+	private String id;
+	private String pw;
+	private String name;
+	private int type; // データ型何？
+	private String ins_pw;
+
+	// 引数があるコンストラクタ
+	public User(String id, String pw, String name, int type, String ins_pw) {
+		super();
+		this.id = id;
+		this.pw = pw;
+		this.name = name;
+		this.type = type;
+		this.ins_pw = ins_pw;
+	}
+
+	// 引数がないコンストラクタ
+	public User() {
+		super();
+		this.id = "";
+		this.pw = "";
+		this.name = "";
+		this.type = 0; // デフォルト値設定
+		this.ins_pw = "";
+	}
+
+	// getter/setterの作成（スコープに入れるとき）
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	public String getIns_pw() {
+		return ins_pw;
+	}
+
+	public void setIns_pw(String ins_pw) {
+		this.ins_pw = ins_pw;
+	}
 
 }
