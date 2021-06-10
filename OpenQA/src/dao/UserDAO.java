4ebfc18a0f381ab3ef628d9a47b3f4cb4d024361
dojo2021-Kaito <ohigawa@seplus2016.onmicrosoft.com>
2021-06-10 10:43:36 +0900

    UserDAOtabunnkannsei

--- OpenQA/src/dao/UserDAO.java
+++ OpenQA/src/dao/UserDAO.java
@@ -15,7 +15,7 @@ public class UserDAO {
 	public User isLoginOK(String id, String pw) {
 		Connection conn = null;
 		boolean loginResult = false;
-			User user = new User();
+		User user = new User();
 		try {
 			// JDBCドライバを読み込む
 			Class.forName("org.h2.Driver");
@@ -111,4 +111,52 @@ public class UserDAO {
 		return result;
 	}
 
+	//PW再設定
+	public boolean update(String pw) {
+		Connection conn = null;
+		boolean result = false;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/OpenQA", "sa", "");
+
+			// SQL文を準備する
+			String sql = "update user set pw=?";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を完成させる
+			pStmt.setString(1, pw);
+			// SQL文を実行する
+			int num = pStmt.executeUpdate();
+			if(num == 1) {
+				result = true;
+			}else {
+				result = false;
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		// 結果を返す
+		return result;
+	}
 }
+
