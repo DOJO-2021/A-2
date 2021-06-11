16e35865795968985fd9c4c82b023e0e2c1b2802
dojo2021-A2-sawaragi <sawaragi@seplus2016.onmicrosoft.com>
2021-06-11 13:47:48 +0900

    質問投稿

--- OpenQA/WebContent/WEB-INF/jsp/questionPost.jsp
+++ OpenQA/WebContent/WEB-INF/jsp/questionPost.jsp
@@ -5,6 +5,7 @@
 <head>
 <meta charset="UTF-8">
 <title>Open Q&A System</title>
+<script type="text/javascript" src="/OpenQA/js/common.js"></script>
 </head>
 <body>
 <h2>質問登録</h2>
@@ -17,9 +18,26 @@
 			<option value="1">講師</option>
 			<option value="2">受講者</option>
 		</select>
+
 		from. ${user.name}
+
 		<input type="hidden" name="anonymity" value="0">
 		<input type="checkbox" name="anonymity" value="1">匿名
+
+		<!-- ひとつめのセレクトボックス -->
+		<!-- web開発コースは保留  -->
+		<select name="b_category">
+			<option value="">カテゴリー</option>
+			<option id="" value="パーソナルスキル">パーソナルスキル</option>
+			<option id="" value="IT基礎">IT基礎コース</option>
+			<option id="" value="java基礎">java基礎コース</option>
+			<option id="" value="その他">その他</option>
+		</select>
+
+		<!-- 二つ目のセレクトボックス（連動する） -->
+		<select name="s_category">
+		</select>
+
 	</th>
 
 
--- OpenQA/WebContent/js/common.js
+++ OpenQA/WebContent/js/common.js
@@ -1,3 +1,35 @@
-/**
- * 
- */
+'use strict'
+
+//ここから変数宣言-----------------------------------------------------------------
+
+	//配列の宣言
+	let s_categoryArray = new Array();
+	//配列の初期値の設定
+	s_categoryArray[''] = new Array('小カテゴリー');
+
+	//ひとつめの配列が選択された後、選択肢によって下記のどれかが設定される
+	s_categoryArray['パーソナルスキル'] = new Array('ビジネスマナー', 'ビジネス文書', 'Word', 'Excel', 'タイピング');
+	s_categoryArray['IT基礎'] = new Array('プログラミングの基本', 'アルゴリズム', 'ネットワークの基本', 'HTML', 'CSS', 'JavaScript', 'データベース', 'SQL');
+	s_categoryArray['java基礎'] = new Array('Javaの基本', 'オブジェクト指向', 'Servlet', 'JSP','DAO', 'Javaドリル', 'SQLドリル', '名刺管理アプリ');
+	s_categoryArray['その他'] = new Array('機器トラブル');
+
+
+	//ここから関数-----------------------------------------------------------------
+	//ひとつめの配列の値が変更されたときに動く関数
+	document.getElementsByName('b_category')[0].onchange = function () {
+		let input = document.getElementsByName('b_category');
+		let b_category = input.children.getAttribute('value');
+		let elm = document.getElementsByName('s_category')[0];
+		elm.options.length = 0;
+		for (let i = 0; i < s_categoryArray[b_category].length; i++) {
+			let op = document.createElement('option');
+			op.value = s_categoryArray[b_category][i];
+			op.textContent = s_categoryArray[b_category][i];
+			elm.appendChild(op);
+		}
+	};
+
+	//JSPの画面が読み込まれたときに実行される関数
+	window.onload = function () {
+	document.getElementsByName('b_category')[0].onchange();
+	};
